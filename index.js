fe8071fa967f76a911c53eabdbebfe459dbbf071
Danny Paul <dannypaulg@gmail.com>
2021-06-13 18:35:24 +0530

    fix(packages): remove vercel as it does not handle dynamic imports

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const core = require('@actions/core');
 const semanticRelease = require('semantic-release');
-require('@semantic-release/commit-analyzer');
-require('@semantic-release/github');
 
 const run = async () => {
   try {
